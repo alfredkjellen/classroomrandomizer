b28a3548d3cdf7dc8d83c54419abfd85c280c30f
Alfred Kjellen <alfred.kjellen@gmail.com>
2024-06-03 20:26:05 +0200

    support

--- src/routes/support/+page.svelte
+++ src/routes/support/+page.svelte
@@ -6,7 +6,7 @@
     <div class="card w-96 bg-base-200 shadow-xl">
         <div class="card-body">
           <h2 class="card-title">Support</h2>
-          <p>If you need support, please send a mail to</p>
+          <p>If you need support, please email</p>
           <p class="text-primary">alfred.kjellen@gmail.com</p>
 
         </div>
