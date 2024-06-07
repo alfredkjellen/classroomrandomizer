75ae13a3f1aadcb1ca79e691a1b1b129b7981537
Alfred Kjellen <alfred.kjellen@gmail.com>
2024-06-07 17:02:07 +0200

    hello

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,11 +7,10 @@
 <footer class="footer items-center p-4 bg-base-300 text-neutral-content">
   <aside class="items-center grid-flow-col">
     <img src="/logo-8.png" alt="Logo" class="w-10">
-    <p>Student Randomizer<br>Made by: <a href="https://github.com/alfredkjellen/studentrandomizer" target="_blank" class="link link-primary">Alfred Kjellén </a></p>
+    <div>Student Randomizer<br>Made by: <i class="text-primary">Alfred Kjellén</i></div>
     
 
   </aside> 
-
   
   <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
     <a href="/support" class="link link-primary">Support</a>
