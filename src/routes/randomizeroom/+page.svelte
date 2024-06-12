83fe1a79c9ff09cca789b0b29dfe72a0381073de
Alfred Kjellen <alfred.kjellen@gmail.com>
2024-06-12 14:27:45 +0200

    svg

--- src/routes/randomizeroom/+page.svelte
+++ src/routes/randomizeroom/+page.svelte
@@ -630,7 +630,7 @@ let isDropdownOpen = false;
       >
         Randomize
         <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
+          class="w-6 h-6 text-gray-800"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
